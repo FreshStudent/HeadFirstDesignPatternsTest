7a4ccf2f490202090d7d329cfc6ae3f34406bdf6
liquanliang <liquanliang@10.20.6.50>
2017-08-24 14:11:37 +0800

    修改文件

--- src/bookCode/ch1/part2/ModelDuck.java
+++ src/bookCode/ch1/part2/ModelDuck.java
@@ -6,11 +6,9 @@ public class ModelDuck extends AbstractDuck{
 	public ModelDuck(){
 		
 		 //鸭子中“叫”的行为，在父类中被委托到iQuackBehavior对象，于是，模型鸭子“叫”实现类是MuteQuackIQuackBehaviorImpl
-		
 		iQuackBehavior = new MuteQuackIQuackBehaviorImpl();  //模型鸭子不会叫
 		
 		//鸭子中“飞”的行为，在父类中被委托到iFlyBehavior对象，于是，模型鸭子“飞”实现类是FlyNoWayIFlyBehaviorImpl
-		
 		iFlyBehavior = new FlyNoWayIFlyBehaviorImpl(); //模型鸭子是不会飞的
 	}
 	
