891698cd52f8cb7dc1109201856564b6f6875c45
liquanliang <liquanliang@10.20.6.50>
2017-08-29 22:25:03 +0800

    change~

--- src/bookCode/ch3/part1/StarbuzzCoffeeMainRun.java
+++ src/bookCode/ch3/part1/StarbuzzCoffeeMainRun.java
@@ -16,21 +16,21 @@ public class StarbuzzCoffeeMainRun {
 		System.out.println(beverage2.getDescription()+" $ "+beverage2.cost());
 		
 		AbstractBeverage beverage3 = new HouseBlendCoffee();  //制造一个HouseBlendCoffee对象
-		beverage3.setSize(Size.MIDDLE); //中杯
+		beverage3.setSize(Size.TALL);	 //小杯
 		beverage3 = new Soy(beverage3);   //用豆浆装饰它
 		beverage3 = new Mocha(beverage3); //用摩卡装饰它
 		beverage3 = new Whip(beverage3);  //用奶泡装饰它
 		System.out.println(beverage3.getDescription()+" $ "+beverage3.cost());
 		
 		AbstractBeverage beverage4 = new DecafCoffee();  //制造一个DecafCoffee对象
-		beverage4.setSize(Size.MIDDLE); //中杯
+		beverage4.setSize(Size.MIDDLE);   //中杯
 		beverage4 = new Soy(beverage4);   //豆浆
 		beverage4 = new Mocha(beverage4); //摩卡
 		beverage4 = new Whip(beverage4);  //奶泡
 		System.out.println(beverage4.getDescription()+" $ "+beverage4.cost());
 		
 		AbstractBeverage beverage5 = new DecafCoffee();  //制造一个DecafCoffee对象
-		beverage5.setSize(Size.LARGE); // 大杯
+		beverage5.setSize(Size.LARGE);    //大杯
 		beverage5 = new Soy(beverage5);   //豆浆
 		beverage5 = new Mocha(beverage5); //摩卡
 		beverage5 = new Whip(beverage5);  //奶泡
